8354b374708d2743be03aa970737c5f249f1095a
Michael Luo <mluo@seven.com.au>
2018-12-11 14:24:09 +1100

    update

--- Ranking Poker Hands/PokerHand.js
+++ Ranking Poker Hands/PokerHand.js
@@ -1,9 +1,21 @@
 var Result = { win: 1, loss: 2, tie: 3 };
 var numbers = { T: 10, J: 11, Q: 12, K: 13, A: 14 };
-var suits = { S: 1, H: 2, D: 3, C: 4 };
+var suits = ["S", "H", "D", "C"];
+var combinations = [
+  "Royal Flush",
+  "Straight Flush",
+  "Four of a Kind",
+  "Full House",
+  "Flush",
+  "Straight",
+  "Three of a Kind",
+  "Two Pair",
+  "One Pair"
+];
 
+function checkRoyalFlush(cards) {}
 function PokerHand(hand) {
-  let hands = hand.split("");
+  var cards = hand.split("");
 }
 
 PokerHand.prototype.compareWith = function(hand) {
